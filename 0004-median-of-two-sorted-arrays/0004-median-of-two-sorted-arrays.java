4d17b186d27dd53dca079b37a79c2cbb2d78b22f
johnkrupakar <67743005+johnkrupakar@users.noreply.github.com>
2024-02-07 09:13:11 +0530

    Time: 1 ms (100.00%), Space: 46.1 MB (38.44%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -3,53 +3,39 @@ class Solution {
         //without using extra space
         int n1 = nums1.length;
         int n2 = nums2.length;
-        
-        //total size
-        int n = n1 + n2;
-        
-        //required indices
-        int ind2 = n/2;
-        int ind1 = ind2 - 1;
-        int cnt = 0;
-        int ind1element = -1;
-        int ind2element = -1;
-        
-        //apply merge sort
-        int i=0;
-        int j=0;
-        while(i< n1 && j < n2){
-            if(nums1[i] < nums2[j]){
-                if(cnt == ind1) ind1element = nums1[i];
-                if(cnt == ind2) ind2element = nums1[i];
-                cnt++;
-                i++;
+
+        //if n1 is bigger swap the arrays:
+        if(n1 > n2) return findMedianSortedArrays(nums2, nums1);
+        int n = n1 + n2; //total length
+        int left = (n1 + n2 +1)/2;    //length of left half
+
+        int low = 0;
+        int high = n1;
+        while(low <= high){
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1;
+
+            //calculate l1, l2, r1 and r2;
+            int l1 = (mid1 > 0) ? nums1[mid1 - 1] : Integer.MIN_VALUE;
+            int l2 = (mid2 > 0) ? nums2[mid2 - 1] : Integer.MIN_VALUE;
+            int r1 = (mid1 < n1) ? nums1[mid1] : Integer.MAX_VALUE;
+            int r2 = (mid2 < n2) ? nums2[mid2] : Integer.MAX_VALUE;
+
+            if(l1 <= r2 && l2 <= r1){
+                if(n % 2 == 1){
+                    return Math.max(l1,l2);
+                }
+                else{
+                    return ((double)(Math.max(l1,l2) + Math.min(r1,r2))) / 2.0;
+                }
+            }
+            else if(l1 > r2){
+                high = mid1 - 1;
             }
             else{
-                if(cnt == ind1) ind1element = nums2[j];
-                if(cnt == ind2) ind2element = nums2[j];
-                cnt++;
-                j++;
+                low = mid1 + 1;
             }
         }
-        
-        while(i<n1){
-            if(cnt == ind1) ind1element = nums1[i];
-            if(cnt == ind2) ind2element = nums1[i];
-            cnt++;
-            i++;
-        }
-        
-        while(j<n2){
-            if(cnt == ind1) ind1element = nums2[j];
-            if(cnt == ind2) ind2element = nums2[j];
-            cnt++;
-            j++;
-        }
-        
-        //Find the median:
-        if(n%2 == 1){
-            return (double)ind2element;
-        }
-        return (double)((double)(ind1element + ind2element)) / 2.0;
+        return 0; //dummy statement
     }
 }
